a219e71294530d9b3a67de110dac9e76150a97b8
Christerle Nounoute <christerlesn@gmail.com>
2020-08-30 20:04:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-  elsif "#{phrase}".downcase!
+  if "#{phrase}".downcase!
     return "HUH?! SPEAK UP, SONNY!"
   else "#{phrase}".upcase!
     return "NO, NOT SINCE 1938!"
